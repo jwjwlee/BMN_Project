390e26ed0a873d08849adbb9315f48c1fdbbcaf7
leejung <ronlystar@hanmail.net>
2017-01-19 15:20:10 +0900

    Test-이정

---
+++ app/src/main/java/com/jungwonlee/bmn_project/Init_Activity.java
@@ -0,0 +1,13 @@
+package com.jungwonlee.bmn_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Init_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init_);
+    }
+}
