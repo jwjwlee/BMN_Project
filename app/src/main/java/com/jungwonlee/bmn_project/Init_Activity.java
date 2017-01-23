a42ebc81ca0f8577a86f8e86ab6b29cb2e906e11
Minsoo <pmmms1226@gmail.com>
2017-01-24 05:00:16 +0900

    [선형레이아웃으로 변경 , 네비 버튼 클릭시 엑티비티 전환]

--- app/src/main/java/com/jungwonlee/bmn_project/Init_Activity.java
+++ app/src/main/java/com/jungwonlee/bmn_project/Init_Activity.java
@@ -1,7 +1,9 @@
 package com.jungwonlee.bmn_project;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Init_Activity extends Activity {
 
@@ -9,5 +11,9 @@ public class Init_Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init_);
+    }
+    public void onButtonNavi(View v){
+         Intent intent  = new Intent(getApplicationContext(), navi_Activity.class);
+         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/jungwonlee/bmn_project/navi_Activity.java
@@ -0,0 +1,13 @@
+package com.jungwonlee.bmn_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class navi_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navi);
+    }
+}
