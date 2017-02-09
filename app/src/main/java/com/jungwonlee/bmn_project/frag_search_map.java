cf3a5bd749f73da0e264da4d42bfd189f56487e6
leejung <ronlystar@hanmail.net>
2017-02-09 11:44:03 +0900

    에러 해결(이미지 버튼)

--- app/src/main/java/com/jungwonlee/bmn_project/frag_search_map.java
+++ app/src/main/java/com/jungwonlee/bmn_project/frag_search_map.java
@@ -6,7 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
 import com.skp.Tmap.TMapView;
@@ -20,17 +20,18 @@ public class frag_search_map extends Fragment{
     @Nullable
     @Override
 
-    public View onCreateView(LayoutInflater inflater,        ViewGroup container, Bundle savedInstanceState) {
-            ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.frag_search_map, container, false);
-            //현재 위치로 초기화 해서 맵 보여주기.
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.frag_search_map, container, false);
+
+        //현재 위치로 초기화 해서 맵 보여주기.
         final Search activity = (Search) getActivity();
         final RelativeLayout relativeLayout = (RelativeLayout) rootView.findViewById(R.id.search_map_view);
         TMapView tmapview = activity.ViewMap();
-        relativeLayout.addView(tmapview);
+        relativeLayout.addView(tmapview, 0);
 
-            //현재위치로 화면을 옮기기
-            Button cur_search_button = (Button) rootView.findViewById(R.id.startNavi);
-            cur_search_button.setOnClickListener(new View.OnClickListener() {
+        //현재위치로 화면을 옮기기
+        ImageButton cur_search_button = (ImageButton) rootView.findViewById(R.id.imageButton);
+        cur_search_button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                    activity.CurLoc();
